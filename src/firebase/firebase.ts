d3915d61a5ff87b0f53afb73248a82b626863a5b
Darian Esquivel <darianesquivelf@gmail.com>
2025-08-05 22:36:12 -0300

    updated key .env

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,23 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  collection,
-  getFirestore,
-  getDocs,
-  addDoc,
-} from "firebase/firestore/lite";
-import { PRODUCTS_A } from "../data/products";
+import { getFirestore } from "firebase/firestore/lite";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAWyUlqc7S1iWEsfPOF5qX6sQSUZAvlfzk",
-  authDomain: "lasreliquiasdelte-2a95e.firebaseapp.com",
-  projectId: "lasreliquiasdelte-2a95e",
-  storageBucket: "lasreliquiasdelte-2a95e.firebasestorage.app",
-  messagingSenderId: "662525584216",
-  appId: "1:662525584216:web:f25e28064431a6bb137b2d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -29,7 +24,7 @@ export const db = getFirestore(app);
 //     const productsCol = collection(db, "products");
 
 //     for (const product of PRODUCTS_A) {
-//       const { id, ...productData } = product; // saco el id para que Firebase cree uno nuevo
+//       const { id, ...productData } = product;
 //       await addDoc(productsCol, productData);
 //     }
 
