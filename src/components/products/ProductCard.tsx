66277c28500401f0315c73a0f8e912cf01b50acb
Darian Esquivel <darianesquivelf@gmail.com>
2025-08-18 22:58:55 -0300

    update card view

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -7,6 +7,7 @@ import {
   Dialog,
   IconButton,
   VisuallyHidden,
+  Box,
 } from "@radix-ui/themes";
 import { type Product } from "../../types/product";
 import { ProductDetail } from "./ProductDetail";
@@ -54,92 +55,96 @@ export const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger>
-        <Card>
-          <Flex gap="4" width="350px" height="95px" align="center">
-            {/* IMAGE */}
-            <Flex width="30%">
-              <Avatar size="7" fallback src={imageUrl} />
-            </Flex>
+        <Box width="100%">
+          <Card>
+            <Flex gap="4" align="center">
+              {/* IMAGE */}
+              <Flex>
+                <Avatar size="7" fallback src={imageUrl} />
+              </Flex>
 
-            {/* NAME - DESCRIPTION - PRICE - PRICE DISCOUNT */}
-            <Flex direction="column" gap="1" width="50%">
-              {category && (
-                <Badge className="max-w-min" color={category.color} size="1">
-                  {category.name}
-                </Badge>
-              )}
-
-              <Text
-                className="line-clamp-1 text-ellipsis"
-                size="2"
-                weight="bold"
-              >
-                {name}
-              </Text>
-              <Text
-                className="line-clamp-2  text-ellipsis"
-                size="1"
-                weight="light"
-              >
-                {description}
-              </Text>
-
-              <Flex gap="2">
-                {price_discount && (
-                  <Text color="green" weight="bold" size="1">
-                    ${price_discount}
-                  </Text>
+              {/* NAME - DESCRIPTION - PRICE - PRICE DISCOUNT */}
+              <Flex direction="column" gap="1">
+                {category && (
+                  <Badge className="max-w-min" color={category.color} size="1">
+                    {category.name}
+                  </Badge>
                 )}
 
                 <Text
-                  className={price_discount ? "line-through text-gray-400" : ""}
-                  weight={price_discount ? "light" : "bold"}
-                  size="1"
+                  className="line-clamp-1 text-ellipsis"
+                  size="2"
+                  weight="bold"
                 >
-                  ${price}
+                  {name}
                 </Text>
-              </Flex>
-            </Flex>
+                <Text
+                  className="line-clamp-2  text-ellipsis"
+                  size="1"
+                  weight="light"
+                >
+                  {description}
+                </Text>
 
-            {/* TAGS */}
-            <Flex gap="2" direction="column" align="end" width="20%">
-              {isNewProduct && (
-                <Badge className="max-w-min" color="mint" radius="full">
-                  New
-                </Badge>
-              )}
-
-              {percentDiscount && (
-                <Badge color="orange" radius="full">
-                  {percentDiscount}% OFF
-                </Badge>
-              )}
-
-              <Flex direction="column" gap="1">
-                <Flex justify="end" gap="1">
-                  {isVegetarian && (
-                    <FontAwesomeIcon color="orange" icon={faCarrot} />
-                  )}
-
-                  {isVegan && <FontAwesomeIcon color="green" icon={faLeaf} />}
-                </Flex>
-                <Flex justify="end" gap="2">
-                  {isGlutenFree && (
-                    <Text color="lime" size="2" weight="bold">
-                      GF
+                <Flex gap="2">
+                  {price_discount && (
+                    <Text color="green" weight="bold" size="1">
+                      ${price_discount}
                     </Text>
                   )}
 
-                  {isLactoseFree && (
-                    <Text color="sky" size="2" weight="bold">
-                      LF
-                    </Text>
-                  )}
+                  <Text
+                    className={
+                      price_discount ? "line-through text-gray-400" : ""
+                    }
+                    weight={price_discount ? "light" : "bold"}
+                    size="1"
+                  >
+                    ${price}
+                  </Text>
+                </Flex>
+              </Flex>
+
+              {/* TAGS */}
+              <Flex gap="2" direction="column" align="end">
+                {isNewProduct && (
+                  <Badge className="max-w-min" color="mint" radius="full">
+                    New
+                  </Badge>
+                )}
+
+                {percentDiscount && (
+                  <Badge color="orange" radius="full">
+                    {percentDiscount}% OFF
+                  </Badge>
+                )}
+
+                <Flex direction="column" gap="1">
+                  <Flex justify="end" gap="1">
+                    {isVegetarian && (
+                      <FontAwesomeIcon color="orange" icon={faCarrot} />
+                    )}
+
+                    {isVegan && <FontAwesomeIcon color="green" icon={faLeaf} />}
+                  </Flex>
+                  <Flex justify="end" gap="2">
+                    {isGlutenFree && (
+                      <Text color="lime" size="2" weight="bold">
+                        GF
+                      </Text>
+                    )}
+
+                    {isLactoseFree && (
+                      <Text color="sky" size="2" weight="bold">
+                        LF
+                      </Text>
+                    )}
+                  </Flex>
                 </Flex>
               </Flex>
             </Flex>
-          </Flex>
-        </Card>
+          </Card>
+        </Box>
       </Dialog.Trigger>
       <Dialog.Content aria-describedby={undefined}>
         <Dialog.Title>
